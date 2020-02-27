68b48f9c3b89c9ebb7fe4cc2a8f5bd029cc7fadc
yushiyuan <yushiyuan@didiglobal.com>
2020-02-27 17:50:26 +0800

    update tmux config

--- tmux.sh
+++ tmux.sh
@@ -1,2 +1,4 @@
-ln -s -f ./tmux.conf ~/.tmux.conf
-ln -s -f ./tmux.conf.local ~/.tmux.conf.local
+work_path=$(pwd)
+echo ${work_path}
+ln -s -f ${work_path}/tmux.conf ~/.tmux.conf
+ln -s -f ${work_path}/tmux.conf.local ~/.tmux.conf.local
