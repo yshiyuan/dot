0a666531f40c75030bf9e29e5be99aec51f11060
yushiyuan <yushiyuan@didiglobal.com>
2020-10-29 12:02:15 +0800

    mkdir tmux vim zsh

---
+++ conf.sh
@@ -0,0 +1,10 @@
+cur_path=$(pwd)
+echo "当前路径:"${cur_path}
+
+#tmux配置
+ln -s -f ${cur_path}/tmux/tmux.conf $HOME/.tmux.conf
+ln -s -f ${cur_path}/tmux/tmux.conf.local $HOME/.tmux.conf.local
+
+#zsh配置
+mkdir -p $HOME/.ytrash #创建回收站
+ln -s -f ${cur_path}/zsh/zshrc $HOME/.zshrc
--- tmux.sh
+++
@@ -1,4 +0,0 @@
-work_path=$(pwd)
-echo ${work_path}
-ln -s -f ${work_path}/tmux.conf ~/.tmux.conf
-ln -s -f ${work_path}/tmux.conf.local ~/.tmux.conf.local
