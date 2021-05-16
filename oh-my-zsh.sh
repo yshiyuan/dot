f3cc6670de096f5c9d189a209cee7f93d41ec505
yshiyuan <shiyuan.yu@outlook.com>
2021-05-16 15:10:25 +0800

    fix path

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,3 +1,3 @@
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
