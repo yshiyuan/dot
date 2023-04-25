a9c5578170e680bb7287cd9f5135121e6e467690
yushiyuan <yushiyuan@didiglobal.com>
2023-04-25 17:15:13 +0800

    update from ubunut

--- conf.sh
+++ conf.sh
@@ -1,4 +1,4 @@
-cur_path=`pwd`
+cur_path=$(pwd)
 echo "当前路径:$cur_path"
 
 #tmux配置
@@ -6,8 +6,8 @@ ln -s -f $cur_path/tmux/tmux.conf $HOME/.tmux.conf
 ln -s -f $cur_path/tmux/tmux.conf.local $HOME/.tmux.conf.local
 
 #zsh配置
-mkdir -p $HOME/.ytrash #创建回收站
-cp $cur_path/zsh/zshrc $HOME/.zshrc
+#mkdir -p $HOME/.ytrash #创建回收站
+#cp $cur_path/zsh/zshrc $HOME/.zshrc
 
 #vim配置
 cp $cur_path/vim/init.vim $HOME/.vimrc
