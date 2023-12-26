2d0a580f2aa6d9903f511afcbc7fd1af47d3d712
yshiyuan <shiyuan.yu@outlook.com>
2023-12-26 14:14:09 +0800

    download the pk10 theme

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,3 +1,4 @@
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
