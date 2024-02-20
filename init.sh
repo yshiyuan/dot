a2239d5ad57b150fa8b46b0e5aba65f4f6690506
yshiyuan <shiyuan.yu@outlook.com>
2024-02-20 16:45:11 +0800

    use submodule .tmux

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ cur_path=$(pwd)
 echo "当前路径:$cur_path"
 
 #tmux配置
-ln -s -f $cur_path/tmux/tmux.conf $HOME/.tmux.conf
-ln -s -f $cur_path/tmux/tmux.conf.local $HOME/.tmux.conf.local
+ln -s -f $cur_path/.tmux/.tmux.conf $HOME/.tmux.conf
+ln -s -f $cur_path/.tmux/.tmux.conf.local $HOME/.tmux.conf.local
 
 #zsh配置
 cp $cur_path/zsh/.zshrc $HOME/.zshrc
